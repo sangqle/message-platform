aa2a42a1d2054b773072d4e56441b38f05bd2cef
sanglq3 <sanglq3@vng.com.vn>
2023-01-15 16:38:23 +0700

    Rename SockerServerApplication to MessageServerApplication and move ApplicationConfiguration to config package

--- chat-service/src/main/java/com/cabin/chat/server/MessageServerApplication.java
+++ chat-service/src/main/java/com/cabin/chat/server/MessageServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SocketServerApplication{
+public class MessageServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SocketServerApplication.class, args);
+        SpringApplication.run(MessageServerApplication.class, args);
         System.out.println("Server is running");
     }
 }
--- chat-service/src/main/java/com/cabin/chat/server/config/ApplicationConfiguration.java
+++ chat-service/src/main/java/com/cabin/chat/server/config/ApplicationConfiguration.java
@@ -1,4 +1,4 @@
-package com.cabin.chat.server;
+package com.cabin.chat.server.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
