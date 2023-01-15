7364fc0c2acf270dd76e67a2b42743d77769a8d6
sanglq3 <sanglq3@vng.com.vn>
2023-01-15 16:34:38 +0700

    refactor structure

--- messages-service/cassandra-spring/src/main/java/com/cabin/data/contact/UserContactRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cabin.data.contact;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface UserContactRepository extends CrudRepository<UserContact, UUID> {
-}
